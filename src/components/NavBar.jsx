e33163d60b9a9a90a97064eddf56df95a96c1d27
Federico Lardone <fedelardone13@gmail.com>
2024-03-13 18:08:59 -0300

    PreEntrega1+Lardone

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => (
+<>
+<div>Ray Ban</div>
+<ul>
+    <li>
+        <a href="#">Productos</a>
+    </li>
+    <li>
+        <a href="#">Nosotros</a>
+    </li>
+    <li>
+        <a href="#">Contacto</a>
+    </li>
+</ul>
+<CartWidget />
+</>
+);
